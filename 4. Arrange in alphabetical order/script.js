db27359193bcb998fe86d5107f50500a54630558
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-04-10 21:23:11 +0530

    updated

--- 4. Arrange in alphabetical order/script.js
+++ 4. Arrange in alphabetical order/script.js
@@ -1,5 +1,26 @@
-const books = {
+const books = ({
     title: "hye how are you",
     author: "Albert  Einstien",
     year: 2000,
-}
+},
+{
+    title: "hye how are you",
+    author: "Albert  Einstien",
+    year: 2000,
+},
+{
+    title: "Pride how are you",
+    author: "Albert  Einstien",
+    year: 2000,
+},
+{
+    title: "How are you?",
+    author: "Albert  Einstien",
+    year: 2000,
+},
+{
+    title: "To kill a Muckingbird",
+    author: "Albert  Einstien",
+    year: 2000,
+})
+
